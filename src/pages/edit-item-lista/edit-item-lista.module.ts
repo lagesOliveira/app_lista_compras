b7120bd766c427a4a0511a53fed08e2b2927eec3
Julio Oliveira <julioflavio.m@gmail.com>
2017-11-07 16:51:44 -0200

    add editpage

---
+++ src/pages/edit-item-lista/edit-item-lista.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EditItemListaPage } from './edit-item-lista';
+
+@NgModule({
+  declarations: [
+    EditItemListaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EditItemListaPage),
+  ],
+})
+export class EditItemListaPageModule {}
