3cc15cae4f5cff852b092bda606536eac79db8e0
Julio Oliveira <julioflavio.m@gmail.com>
2017-11-07 13:34:00 -0200

    item model and edit form

---
+++ src/models/item/item.module.ts
@@ -0,0 +1,5 @@
+export interface Item {
+    id?: string,
+    nome: string,
+    quantidade: number
+}
--- src/pages/add-item-lista/add-item-lista.ts
+++ src/pages/add-item-lista/add-item-lista.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the AddItemListaPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Item } from './../../models/item/item.module';
 
 @IonicPage()
 @Component({
@@ -14,6 +9,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'add-item-lista.html',
 })
 export class AddItemListaPage {
+
+  item: Item = {
+    nome: ' ',
+    quantidade: undefined
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
